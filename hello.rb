a0068c4d2a3794e2357caa7924d880731b26d11d
italo <italomauricio98@gmail.com>
2023-10-08 00:05:59 -0300

    update

--- hello.rb
+++
@@ -1,8 +0,0 @@
-name = gets.chomp
-idade = gets.chomp.to_i
-salario = gets.chomp.to_f
-
-
-puts "Meu nome é #{name}"
-puts "Minha idade é #{idade}"
-puts "Meu salário é #{salario}"
---
+++ testing.rb
@@ -0,0 +1,12 @@
+
+#name = gets.chomp
+#idade = gets.chomp.to_i
+#salario = gets.chomp.to_f
+
+#puts "Meu nome é #{name}"
+#puts "Minha idade é #{idade}"
+#puts "Meu salário é #{salario}"
+
+ruby_hash = {id_name: 'italo', language: 'java', locale: 'brasil'}
+puts ruby_hash
+puts ruby_hash[:locale]
